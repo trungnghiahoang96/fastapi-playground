2e9b46fa24bc524e73dae8c9c20a7dec236594b8
Trung Nghĩa Hoàng <46636991+trungnghiahoang96@users.noreply.github.com>
2023-08-06 22:55:10 +0700

    Update DB setup

--- sqlalchemy2-in-practice/db.py
+++ sqlalchemy2-in-practice/db.py
@@ -1,7 +1,15 @@
 import os
 
 from dotenv import load_dotenv
+from sqlalchemy import MetaData, create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 load_dotenv()
 
 print('Database URL:', os.environ['DATABASE_URL'])
+
+
+load_dotenv()
+
+engine = create_engine(os.environ['DATABASE_URL'])
+Session = sessionmaker(engine)
