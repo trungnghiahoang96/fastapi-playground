293ecffa621390cefe3319daea01324f5041ead9
Trung Nghĩa Hoàng <46636991+trungnghiahoang96@users.noreply.github.com>
2023-08-14 08:24:37 +0700

    Update DB setup

--- sqlalchemy2-in-practice/db.py
+++ sqlalchemy2-in-practice/db.py
@@ -4,9 +4,15 @@ from dotenv import load_dotenv
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-load_dotenv()
 
-print('Database URL:', os.environ['DATABASE_URL'])
+class Model(DeclarativeBase):
+    metadata = MetaData(naming_convention={
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    })
 
 
 load_dotenv()
---
+++ sqlalchemy2-in-practice/models.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+from db import Model
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Product(Model):
+    __tablename__ = 'products'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64), index=True, unique=True)
+    manufacturer: Mapped[str] = mapped_column(String(64), index=True)
+    year: Mapped[int] = mapped_column(index=True)
+    country: Mapped[Optional[str]] = mapped_column(String(32))
+    cpu: Mapped[Optional[str]] = mapped_column(String(32))
+
+    def __repr__(self):
+        return f'Product({self.id}, "{self.name}")'
